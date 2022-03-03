e0208b5b8cc07b2d3106c33bc21ad6fa5d0a5461
KunalR41 <98169502+KunalR41@users.noreply.github.com>
2022-03-03 09:58:54 +0530

    Add files via upload

---
+++ phase 3/Assisted/MicroServivesex/src/test/java/com/example/test/MicroServivesexApplicationTests.java
@@ -0,0 +1,25 @@
+package com.example.test;
+
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+
+class MicroServivesexApplicationTests
+ {
+
+	
+@Test
+	
+void contextLoads() 
+
+{
+
+	
+}
+
+
+}
---
+++ phase 3/Assisted/MicroServivesex0/src/test/java/com/example/test/MicroServivesex0ApplicationTests.java
@@ -0,0 +1,22 @@
+package com.example.test;
+
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+
+class MicroServivesex0ApplicationTests
+ {
+
+@Test
+	
+void contextLoads() 
+{
+	
+}
+
+
+}
